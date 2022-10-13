95787f98dd50546a1766f802d465263d9fabeddc
xdaTq <kujawski.erwin@gmail.com>
2022-10-13 14:18:29 +0200

    fix: docker file issues

--- app/database/config.php
+++ app/database/config.php
@@ -1,11 +1,22 @@
 <?php
-define('DB_SERVER', 'localhost');
+/* localhost
+define('DB_SERVER', 'invetory');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'InventorySys');
- 
-/* Attempt to connect to MySQL database */
+*/
+define('hostname', 'mysql');
+define('username', 'root');
+define('password', 'root');
+define('database', 'InventorySys');
+define('port', '3306'); 
+
+/* Attempt to connect to MySQL database 
+localhost: 
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+*/
+$con = mysqli_connect(hostname, username, password, database, port);
  
 // Check connection
 if($con === false){
